224f925d4954bf97738dddcd07eda3fc380472ce
MulekaevTimur <92471732+MulekaevTimur@users.noreply.github.com>
2021-10-13 21:39:04 +0300

    utils.h

---
+++ utils.h
@@ -0,0 +1,17 @@
+#pragma once
+#include <iostream>
+
+template <typename T>
+T GetCorrectNumber(T min, T max)
+{
+	T x;
+	while ((std::cin >> x).fail()	// check type
+		|| std::cin.peek() != '\n'	// is buffer empty (int/float check)
+		|| x < min || x > max)		// check range
+	{
+		std::cin.clear();
+		std::cin.ignore(10000, '\n');
+		std::cout << "Type number (" << min << "-" << max << "):";
+	}
+	return x;
+}
